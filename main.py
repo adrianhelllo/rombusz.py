121b2941938332efe0ecda3e12a69d15028b1874
adrianhelllo <hetessyadrian@gmail.com>
2025-03-26 09:28:41 +0100

    páratlan szélesség fix

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 def rombusz(szelesseg):
     k = 0
-    while k != szelesseg:
+    while k < szelesseg:
         empty = szelesseg - k - 2
-        print(f"{' '*round((empty/2))}*{k*'-'}{'*' if szelesseg % 2 == 0 else ''}")
-        k += 2
+        print(f"{' '*round((empty/2))}*{k*'-'}{'*' if szelesseg % 2 == 0 else '' if k == 0 else '*'}")
+        if k == 0 and szelesseg % 2 == 1:
+            k = 1
+        else:
+            k += 2
 
     k -= 2
 
     while k >= 0:
         empty = szelesseg - k - 2
-        print(f"{' '*round((empty/2))}*{k*'-'}{'*' if szelesseg % 2 == 0 else ''}")
-        k -= 2
+        print(f"{' '*round((empty/2))}*{k*'-'}{'*' if szelesseg % 2 == 0 else '' if k == 0 else '*'}")
+        if k == 1 and szelesseg % 2 == 1:
+            k = 0
+        else:
+            k -= 2
 
-rombusz(100)
+rombusz(10)
